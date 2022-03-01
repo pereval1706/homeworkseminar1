caa097453868d1b1af4180e6f211964c265c1e19
aleksandra_pereval <perevalova17_06@mail.ru>
2022-03-01 20:59:32 +0900

    1 exercise done

---
+++ Seminar1/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.Write("Введите целое число ");
+int value = Convert.ToInt32(Console.ReadLine());
+int kvadrat_value = value * value;
+Console.Write("Квадрат данного числа равен ");
+Console.WriteLine(kvadrat_value);
