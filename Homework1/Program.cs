2aa6da15f7d45499bbcbda098f9629c69ae1b79f
aleksandra_pereval <perevalova17_06@mail.ru>
2022-03-03 20:48:46 +0900

    homework seminar 1

---
+++ Homework1/Program.cs
@@ -0,0 +1,50 @@
+﻿/*
+//1 задача
+Console.Write("Введите первое число ");
+int A = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число ");
+int B = Convert.ToInt32(Console.ReadLine());
+if(A > B)
+{
+    Console.WriteLine("{0} наибольшее число ",A);
+    Console.WriteLine("{0} наименьшее число ",B);
+}
+else
+{
+    Console.WriteLine("{0} наибольшее число ",B);
+    Console.WriteLine("{0} наименьшее число ",A);
+} 
+
+//2 задача
+Console.Write("Введите три числа ");
+int A = Convert.ToInt32(Console.ReadLine());
+int B = Convert.ToInt32(Console.ReadLine());
+int C = Convert.ToInt32(Console.ReadLine());
+if(A > B && A > C)
+{
+    Console.WriteLine("{0} наибольшее число ",A);
+}
+else if(B > C && B > C)
+{
+    Console.WriteLine("{0} наибольшее число ",B);
+}
+else
+{
+    Console.WriteLine("{0} наибольшее число ",C);
+}
+
+ //3 задача
+
+ Console.Write("Введите число ");
+int A = Convert.ToInt32(Console.ReadLine());
+if (A % 2 == 0)
+{
+    Console.WriteLine("число нечетное");
+           
+}
+else
+{
+    Console.WriteLine("Число четное");
+}
+*/
+
