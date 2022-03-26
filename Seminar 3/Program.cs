3f27ab7a18510afaa6ac8fe457f34a14cabaf486
aleksandra_pereval <perevalova17_06@mail.ru>
2022-03-26 23:11:50 +0900

    Seminas3

---
+++ Seminar 3/Program.cs
@@ -0,0 +1,87 @@
+﻿/*
+// Задача 1
+int GetQuart(double xPoint, double yPoint)
+{
+    if(xPoint > 0 && yPoint > 0) return 1;
+    if(xPoint < 0 && yPoint > 0) return 2;
+    if(xPoint < 0 && yPoint < 0) return 3;
+    if(xPoint > 0 && yPoint < 0) return 4;
+    return 0;
+}
+
+
+double x, y;
+int quartNum;
+Console.Write("Input X: ");
+x = Convert.ToDouble(Console.ReadLine());
+Console.Write("Input Y: ");
+y = Convert.ToDouble(Console.ReadLine());
+
+quartNum = GetQuart(x, y);
+if(quartNum == 0) Console.WriteLine("Нельзя однозначно определить четверть");
+else Console.WriteLine("Данная точка находится в " +  quartNum + " четверти");
+
+
+// Задача 2
+
+void Getret(int numQuad)
+{
+    switch(numQuad)
+    {
+        case 1:
+        {
+             Console.WriteLine("В данной четверти координаты могут быть с след диапозоне: X > 0, Y > 0");
+            break;
+        }
+        case 2:
+        {
+            Console.WriteLine("В данной четверти координаты могут быть с след диапозоне: X < 0, Y > 0");
+            break;
+        }
+        case 3:
+        {
+            Console.WriteLine("В данной четверти координаты могут быть с след диапозоне: X < 0, Y < 0");
+            break;
+        }
+        case 4:
+        {
+            Console.WriteLine("В данной четверти координаты могут быть с след диапозоне: X > 0, Y < 0");
+            break;
+        }
+        default:
+        {
+            Console.WriteLine("Такой четверти не существует");
+            break;
+        }   
+    }
+    
+}
+
+int numGet;
+Console.Write("Введите номер четверти (целое число от 1 до 4): ");
+numGet = Convert.ToInt32(Console.ReadLine());
+Getret(numGet);
+*/
+
+// Задача 3
+
+double GetDistance(double x1, double y1, double x2, double y2)
+{
+    double xDistance = x2 - x1;
+    double yDistance = y2 - y1;
+
+    return Math.Sqrt(Math.Pow(xDistance,2) + Math.Pow(yDistance,2));
+}
+double xA, yA, xB, yB;
+double distance;
+
+Console.Write("Input X of point A: ");
+xA = Convert.ToDouble(Console.ReadLine());
+Console.Write("Input Y of point A: ");
+yA = Convert.ToDouble(Console.ReadLine());
+Console.Write("Input X of point B: ");
+xB = Convert.ToDouble(Console.ReadLine());
+Console.Write("Input Y of point B: ");
+yB = Convert.ToDouble(Console.ReadLine());
+distance = GetDistance(xA, yA, xB , yB);
+Console.WriteLine("Расстояние между точками " + distance);
--- Seminar2/Program.cs
+++ Seminar2/Program.cs
@@ -48,4 +48,26 @@ int number2 = Convert.ToInt32(Console.ReadLine());
 
 Chastnoe(number1, number2);
 
+*/
 
+void Proverka (int num1, int num2, int num3)
+{
+    if(num1 % num2 == 0 && num1 % num3 == 0)
+    {
+        Console.WriteLine("Число " + num1 + " является кратным числам " + num2 + " и " + num3);
+    }
+    else{
+        Console.WriteLine("Число " + num1 + " не является кратным числам " + num2 + " и " + num3);
+    }
+}
+
+Console.WriteLine("Enter first number ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Enter second number ");
+int check1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Enter third number ");
+int check2 = Convert.ToInt32(Console.ReadLine());
+
+Proverka(number1, check1, check2);
